4e11c7c278718f6b4f07b491b15fd5e745e5ec95
kap <william.kapke@gmail.com>
2025-05-17 19:26:20 -0700

    test that the token refresh is working

--- src/connection.ts
+++ src/connection.ts
@@ -140,7 +140,7 @@ export class Connection extends EventEmitter {
   private _cleanBaseURL(url: string): string {
     // Remove query parameters if present
     // url.split always returns at least one element, so we can safely access [0]
-		const parts = url.split("?");
+    const parts = url.split("?");
     const cleanURL = parts[0];
     // Remove trailing slash if present
     return cleanURL.endsWith("/") ? cleanURL.slice(0, -1) : cleanURL;
